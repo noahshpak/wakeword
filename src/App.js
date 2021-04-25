5755c68d3d2fe6946e6f5ce38ae80cd6cba4c3dd
Noah Shpak <noahshpak@gmail.com>
2021-04-25 17:18:06 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Recorder } from 'react-voice-recorder';
+import 'react-voice-recorder/dist/index.css';
+
 import './App.css';
+
+class AudioRecorder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audioDetails: {
+        url: null,
+        blob: null,
+        chunks: null,
+        duration: {
+          h: 0,
+          m: 0,
+          s: 0
+        }
+      }
+    }
+  }
+  handleAudioPause(e) {
+    e.preventDefault();
+    clearInterval(this.timer);
+    this.mediaRecorder.pause();
+    this.setState({ pauseRecord: true });
+  }
+  handleAudioStop(data){
+    console.log(data)
+    this.setState({ audioDetails: data });
+  }
+  handleAudioUpload(file) {
+    console.log(file);
+  }
+  handleReset() {
+    const reset = {
+      url: null,
+      blob: null,
+      chunks: null,
+      duration: {
+        h: 0,
+        m: 0,
+        s: 0
+      }
+    };
+    this.setState({ audioDetails: reset });
+  }
+  render() {
+    return <Recorder
+      record={true}
+      title={"New recording"}
+      audioURL={this.state.audioDetails.url}
+      showUIAudio
+      handleAudioStop={data => this.handleAudioStop(data)}
+      handleAudioUpload={data => this.handleAudioUpload(data)}
+      handleAudioPause={e => this.handleAudioPause(e)}
+      handleReset={() => this.handleReset()}
+    />
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <AudioRecorder />
+      </div>
     </div>
   );
 }
